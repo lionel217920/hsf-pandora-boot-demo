6441ded5f0e5c095d31cc786d6f06365d94cce86
Lionel <lionel217920@163.com>
2021-01-16 22:08:00 +0800

    代码优化

--- hsf-pandora-boot-consumer/src/main/java/com/alibaba/edas/consumer/config/HSFConfig.java
+++ hsf-pandora-boot-consumer/src/main/java/com/alibaba/edas/consumer/config/HSFConfig.java
@@ -2,8 +2,10 @@ package com.alibaba.edas.consumer.config;
 
 import com.alibaba.boot.hsf.annotation.HSFConsumer;
 import com.alibaba.edas.api.HelloWorldService;
+import org.springframework.context.annotation.Configuration;
 
-public class HSFConfig {
+@Configuration
+public class HSFConfig {ls
 
     @HSFConsumer(clientTimeout = 3000, serviceVersion = "1.0.0")
     private HelloWorldService helloWorldService;
